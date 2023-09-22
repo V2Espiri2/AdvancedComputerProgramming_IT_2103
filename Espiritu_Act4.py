e2ecd0032dee8e35304f6c99b33c9acc811c64f4
V2Espiri2 <145733922+V2Espiri2@users.noreply.github.com>
2023-09-22 11:59:39 +0800

    Add files via upload

---
+++ Espiritu_Act4.py
@@ -0,0 +1,23 @@
+print ('Hello Peeps!')
+print ('This is from a line of code')
+print ('\n',5,'\n',5.5,'\n','Hello, World','\n')
+print ('1.', 70+9)
+a=7
+print ('2.',5*a)
+a=2
+b=11
+c=0
+a=a*2
+b=b+(c*3)
+c=a*b
+print('3.',c)
+input_1= int(input('Enter first number: ' ))
+input_2= int(input('Enter second number: ' ))
+print('4.',input_1+input_2)
+Squared, Cubed=3**2, 3**3
+print('5.',Squared,Cubed)
+Name='Gly'
+print('6.','Your name is '+  Name)
+Apple_price=20
+Orange_price=20
+print('7.',Apple_price==Orange_price)
