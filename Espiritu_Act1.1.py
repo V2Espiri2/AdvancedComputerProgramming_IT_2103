5b673b92cfa1b046d5eb596a89ffdb76d798346a
V2Espiri2 <vitosv.espiritu@gmail.com>
2023-09-22 10:50:46 +0800

    Activity 1-2

---
+++ Espiritu_Act1.1.py
@@ -0,0 +1,4 @@
+name = (str)(input("Enter name: "))
+age = (int)(input("Enter age: "))
+add = (str)(input("Enter Address: "))
+print(name, age, add)
---
+++ Espiritu_Act1.2.py
@@ -0,0 +1,5 @@
+name = (str)(input("Enter name: "))
+birthyear = (int)(input("Enter birthyear: "))
+current = 2023
+age = current - birthyear
+print(age)
